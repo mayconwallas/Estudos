b9f1bbe784259ba01970b5610e79931f018d161d
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-01-17 11:32:59 -0300

    commit
    aulas da rocketseat e arrumando arquivos do eu curso

--- eucurso/cursodelogica/semana 02/aula04 conhecimento extra/js.js
+++ eucurso/cursodelogica/semana 02/aula04 conhecimento extra/js.js
@@ -1,38 +1,33 @@
 function calculo() {
-
-        //alert('vamos usar a calculadora.')
-        /* let operador = window.prompt('Digite a operação deseja realizar:'+'\n'+'somar'+'\n'+'subtrair'+'\n'+'dividir'+'\n'+'multiplicar') */
-       // num01 = window.prompt('escolha o primeiro numero:')
-       // num02 = window.prompt('escolha o segundo numero:')
-
     
-       let select = document.querySelector('#operador');
-       let operador = select.options[select.selectedIndex].value;
-       let num01 = document.querySelector('#pnum')
-       let num02 = document.querySelector('#snum')
-       
-
-        if (operador == 'somar' || operador == '+' ) {
-            let res = parseFloat(num01) + parseFloat(num02)
-            alert(`${text}A soma de ${num01} e ${num02} é ${res}.`)            
-        } else if (operador == 'subtrair' || operador == '-'  ) {
-            num01 = window.prompt('escolha o primeiro numero:')
-            num02 = window.prompt('escolha o segundo numero:')
-            let res = num01 - num02
-            alert(`A subtração de ${num01} e ${num02} é ${res}.`)
-        } else if (operador == 'dividir' || operador == '/'  ) {
-            num01 = window.prompt('escolha o primeiro numero:')
-            num02 = window.prompt('escolha o segundo numero:')
-            let res = num01 / num02
-            alert(`A divisão de ${num01} e ${num02} é ${res}.`)
-        } else if (operador == 'multiplicar' || operador == '*'  ) {
-            num01 = window.prompt('escolha o primeiro numero:')
-            num02 = window.prompt('escolha o segundo numero:')
-            let res = num01 * num02
-            alert(`A multiplicação de ${num01} e ${num02} é ${res}.`)
-        } else {
-            alert('escolha o operador correto.')
-        } 
-
+       var operador = document.getElementById('oper')
+       var num01 = document.getElementById('pnum')
+       var num02 = document.getElementById('snum')
+       window.alert(operador)
+       window.alert(num01)
+       window.alert(num02)
+       if (operador == 'somar' || operador == '+' ) {
+           num01 = window.prompt('escolha o primeiro numero:')
+           num02 = window.prompt('escolha o segundo numero:')
+           let res = parseFloat(num01) + parseFloat(num02)
+           alert(`A soma de ${num01} e ${num02} é ${res}.`)            
+       } else if (operador == 'subtrair' || operador == '-'  ) {
+           num01 = window.prompt('escolha o primeiro numero:')
+           num02 = window.prompt('escolha o segundo numero:')
+           let res = num01 - num02
+           alert(`A subtração de ${num01} e ${num02} é ${res}.`)
+       } else if (operador == 'dividir' || operador == '/'  ) {
+           num01 = window.prompt('escolha o primeiro numero:')
+           num02 = window.prompt('escolha o segundo numero:')
+           let res = num01 / num02
+           alert(`A divisão de ${num01} e ${num02} é ${res}.`)
+       } else if (operador == 'multiplicar' || operador == '*'  ) {
+           num01 = window.prompt('escolha o primeiro numero:')
+           num02 = window.prompt('escolha o segundo numero:')
+           let res = num01 * num02
+           alert(`A multiplicação de ${num01} e ${num02} é ${res}.`)
+       } else {
+           alert('escolha o operador correto.')
+       }
 
     }  
