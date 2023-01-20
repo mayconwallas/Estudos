2efe1a366fa37e1881ab0307acf2d7370b832e36
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-01-20 17:55:21 -0300

    commit
    estudando java curso em video desde o inicio com anotações

---
+++ CursoEmVideo/JavaScript/aulas/moduloB/js.js
@@ -0,0 +1,32 @@
+// vamos ter varias praticas
+
+/* Pratica 01
+let mensagem = "Olá, Mundo!"
+let mensagem02 = "O que esta achando do conteudo?"
+window.alert(mensagem)
+window.confirm(mensagem02)
+let mensagem03 = window.prompt("Escreva alguma coisa")
+window.alert(`Você Escreveu "${mensagem03}".`)
+*/
+
+//  pratica 02 
+//let nome = window.prompt("Qual seu nome?")
+//window.alert(`é um grande prazer em te conhecer ${nome}`)
+
+// pratica 03
+/*let sair = "não"
+while (sair != "s") {
+    let res = 1
+let n1 = Number(window.prompt("Digite um numero:"))
+let n2 = Number(window.prompt("Digite outro numero:"))
+let operador = prompt("Você quer somar '+' ou subtrair '-' ?")
+    if (operador == "somar" || operador == "+") {
+            res = n1 + n2
+            alert(`o resultado da some de ${n1} + ${n2} é ${res}`)       
+    } else if (operador == "subtrair" || operador == "-"){
+            res = n1 - n2
+            alert(`o resultado da subtração de ${n1} - ${n2} é ${res}`)
+    } 
+    sair = prompt("para sair digite s ou para continhar digite c")
+}
+ */
