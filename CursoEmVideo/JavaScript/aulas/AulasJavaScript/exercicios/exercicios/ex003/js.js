811b470be9ef079a4faae6d77b25d00a122b2648
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-01-23 17:39:37 -0300

    add
    aquivos adicionados

---
+++ CursoEmVideo/JavaScript/aulas/AulasJavaScript/exercicios/exercicios/ex003/js.js
@@ -0,0 +1,4 @@
+function inicio() {
+    let nome = window.prompt('Qual seu nome? ')
+    window.alert(`Olá, ${nome} é um prazer conhecer você.`)
+}
---
+++ CursoEmVideo/JavaScript/aulas/AulasJavaScript/exercicios/modelo/04contador.js
@@ -0,0 +1,29 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
+        //window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = `Impossível contar!`
+    } else {
+        res.innerHTML = `Contando:`
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0){
+            window.alert('passo inválido! considerando PASSO 1!')
+            p = 1
+        }
+        if(i < f){
+            for (let c = i;  c <= f; c += p){
+                res.innerHTML += `${c}, `      
+            } 
+        } else{
+            for (let c = i;  c >= f; c -= p){
+                res.innerHTML += `${c}, `      
+            } 
+        }
+    }
+}
