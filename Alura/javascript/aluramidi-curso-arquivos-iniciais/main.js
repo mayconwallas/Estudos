1e580b11de5b41d5e06284f0750b1baacc312cd1
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-02-15 18:11:27 -0300

    curso de javascript finalizado
    mais um curso finalizado o aluramidi

--- Alura/javascript/aluramidi-curso-arquivos-iniciais/main.js
+++ Alura/javascript/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,18 +1,31 @@
+function tocaSom (idElementoAudio) {
+   const elemento = document.querySelector(seletorAudio); 
 
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play(); 
+    if (elemento && elemento.localName === 'audio') {
+            elemento.play();
+            
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido.')
+    }
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;  
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play(); 
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;  
-
-function tocaSomTim () {
-    document.querySelector('#som_tecla_tim').play(); 
-}
-
-document.querySelector('.tecla_tim').onclick = tocaSomTim; 
