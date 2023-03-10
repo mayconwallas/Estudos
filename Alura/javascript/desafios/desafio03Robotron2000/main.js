ce42495cf0d5ad27afa10f6a567cc4bd4c796123
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-03-10 12:04:23 -0300

    curso de DOM

---
+++ Alura/javascript/desafios/desafio03Robotron2000/main.js
@@ -0,0 +1,6 @@
+var click = document.querySelector("#calcular");
+var resultado = document.querySelector(".resultado");
+
+click.addEventListener("click", (evento) => {
+    resultado.innerHTML = "Fui clicado"
+});
