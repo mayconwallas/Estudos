17cbcacb9c15469b9281284b00fbb52e7f74c5e4
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-03-14 14:55:20 -0300

    arrumando itens

--- Alura/javascript/06 numeroSecreto/app/reconhecimentoDeVoz.js
+++ Alura/javascript/06 numeroSecreto/app/reconhecimentoDeVoz.js
@@ -3,26 +3,22 @@ const elementoChute = document.getElementById('chute')
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-
 recognition.lang = 'pt-Br'
-recognition.start();
+recognition.start()
 
-recognition.addEventListener('result', onSpeak);
-
-
-
+recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e) {
-    chute = e.results[0][0].transcript;
-    exibeChuteNaTela(chute);
-    verificaSeOChutePossuiUmValorValido(chute);
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+    verificaSeOChutePossuiUmValorValido(chute)
 }
 
 function exibeChuteNaTela(chute) {
     elementoChute.innerHTML = `
         <div>Você disse</div>
         <span class="box">${chute}</span>
-    `
+     `
 }
 
-recognition.addEventListener('end', () => recognition.start());
+recognition.addEventListener('end', () => recognition.start())
