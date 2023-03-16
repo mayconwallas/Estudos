65b28a71a5cd58bcd56d1a2cadbdf507656f0251
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-03-16 18:11:51 -0300

    commit estudando javascript arrays

---
+++ Alura/javascript/01 trilhaJavaScriptBasica/01JavaScriptArrays/02ComoFuncionaArrays.js
@@ -0,0 +1,21 @@
+//como funcinam os arrays
+
+//notas 10  6.5  8  7.5 <= elementos/ itens/ valores
+//       0   1   2   3   <= índices / posições
+
+//criando um array
+const notas = [10, 6.5, 7.5, 10]; 
+
+//impimindo o item na posição 2
+console.log(notas[2]);// imprime o número 7.5
+
+
+//tambem podemos colocar outros valores no array
+
+//outrosDados 10  "oi" true  false <= elementos/ itens/ valores
+//             0   1    2      3   <= índices / posições
+
+const outrosDados = [10, "oi", true, false];
+console.log(outrosDados[2]);// imprime o true
+
+/* é uma boa pratica trabalhar um mesmo tipo de dados no mesmo array*/ 
---
+++ Alura/javascript/01 trilhaJavaScriptBasica/01JavaScriptArrays/04DeletandoElementos.js
@@ -0,0 +1,25 @@
+
+const notas = [10, 6, 8, 5.5, 10];
+const media = (notas[0] + notas[1] + notas[2]) / notas.length
+console.log(media);
+
+
+// para deletar o ultimo valores no arrays 
+const notas1 = [10, 6, 8, 5.5, 10];
+notas1.pop();
+const media1 = (notas1[0] + notas1[1] + notas1[2] + notas1[3]) / notas.length;
+console.log(`A média é ${media1}.`);
+
+
+
+
+
+
+
+
+/*const notas1 = [10, 6, 8, 5.5, 10];
+const indice = notas1.indexOf(10);
+notas1.slice(indice, 1);
+const media1 = (notas1[0] + notas1[1] + notas1[2] + notas1[3]) / notas.length
+console.log(media1);*/
+
