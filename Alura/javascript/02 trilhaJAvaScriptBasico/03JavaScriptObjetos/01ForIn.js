417444137d2eb7fcd0000f11eb09f6652efbf788
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-03-21 14:38:17 -0300

    arrumando arquivos

---
+++ Alura/javascript/02 trilhaJAvaScriptBasico/03JavaScriptObjetos/01ForIn.js
@@ -0,0 +1,20 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555550", "1144444440"],
+    saldo: 200,
+};
+  
+cliente.enderecos = [
+    {
+        rua: "R. Joseph Climber",
+        numero: 1337,
+        apartamento: true,
+        complemento: "ap 934"
+    },
+];
+
+for (let chave in cliente){
+    console.log(chave)
+}
