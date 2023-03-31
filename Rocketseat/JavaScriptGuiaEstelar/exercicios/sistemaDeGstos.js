3575e4c15225ba0ab443b894ed70fbb96f3c2a3e
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-03-31 13:42:04 -0300

    concluido mais um desafio

--- Rocketseat/JavaScriptGuiaEstelar/exercicios/sistemaDeGstos.js
+++ Rocketseat/JavaScriptGuiaEstelar/exercicios/sistemaDeGstos.js
@@ -1,3 +1,26 @@
-const receita  = [4000, 1500];
-const gastos = [1500, 1200, 300, 200, 500, 250, 100, 25];
+familia = {
+  receita:[4000, 1500],
+  gastos:[1500, 1200, 300, 200, 500, 250, 100, 25]
+}
+
+const receitaSeparada  = familia.receita;
+const gastosSeparado = familia.gastos;
+
+function somarArray(arr) {
+    return arr.reduce((total, valor) => total + valor, 0);
+}
+
+let receitaTotal = somarArray(receitaSeparada);
+let gastoTotal = somarArray(gastosSeparado);
+
+
+function comparaValores(receita, gastos) {
+    return receita - gastos;
+}
+
+console.log(`Receira: ${receitaTotal}, Gastos: ${gastoTotal}`);
+let saldoConta = comparaValores(receitaTotal, gastoTotal);
+console.log(`Seu saldo é: ${saldoConta}`);
+
+
 
