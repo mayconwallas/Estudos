1ba55eb5536906ccbfaadbe9849aeaf99ddf244e
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-04-01 19:31:38 -0300

    fazendo alguns testes de js

---
+++ Rocketseat/JavaScriptGuiaEstelar/exercicios/teste.js
@@ -0,0 +1,23 @@
+const pessoa = {
+    pessoa1: {
+      nome: "João",
+      idade: 25,
+      profissao: "Desenvolvedor",
+      cidade: "São Paulo"
+    },
+    pessoa2: {
+      nome: "Maria",
+      idade: 32,
+      profissao: "Enfermeira",
+      cidade: "Rio de Janeiro"
+    },
+    pessoa3: {
+      nome: "Pedro",
+      idade: 42,
+      profissao: "Advogado",
+      cidade: "Porto Alegre"
+    }
+  };
+
+
+  console.log();
