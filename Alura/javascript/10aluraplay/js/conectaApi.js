9c316a70ab55a09015640ff15601944193fdf137
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-04-02 12:05:10 -0300

    finalizado curso consumindo apis

--- Alura/javascript/10aluraplay/js/conectaApi.js
+++ Alura/javascript/10aluraplay/js/conectaApi.js
@@ -5,11 +5,11 @@ async function listaVideos() {
     return conexaoConvertida;
 }
 
-async function criaVideo() {
+async function criaVideo(titulo, descricao, url, imagem) {
     const conexao = await fetch("http://localhost:3000/videos", {
         method: "POST",
-        headers:{
-            "content-type": "application/json"
+        headers: {
+            "Content-type": "application/json"
         },
         body: JSON.stringify({
             titulo: titulo,
@@ -18,12 +18,22 @@ async function criaVideo() {
             imagem: imagem
         })
     });
+    if (!conexao.ok) {
+        throw new Error("Não foi possível enviar o video");
+    }
+    const conexaoConvertida = conexao.json();
+    return conexaoConvertida;
+}
 
-    const conexaoConvertida = await conexao.json();
-        return conexaoConvertida;
+async function buscaVideos(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`)
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
 }
 
 export const conectaApi = {
     listaVideos,
-    criaVideo
+    criaVideo,
+    buscaVideos
 }
