2adb6cea15cc83bec7526c0827ae2293089d2d1b
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-04-04 20:18:33 -0300

    fazendo exercicios de javascripti

---
+++ Alura/javascript/desafios/desafio00/main.js
@@ -0,0 +1,13 @@
+function soma(x, y) {
+    return x + y;
+}
+
+function tamanho(informacao) {
+    return informacao.length;
+}
+
+function maior(arr) {
+    
+}
+
+console.log(tamanho([4, 10, 2, 8]))
