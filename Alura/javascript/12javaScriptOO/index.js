2659240dc278023c56cdd9dca86d044551cfb28c
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-04-06 13:51:42 -0300

    progredindo js oo

--- Alura/javascript/12javaScriptOO/index.js
+++ Alura/javascript/12javaScriptOO/index.js
@@ -1,23 +1,53 @@
-const cliente1Nome = "Ricardo";
-const cliente1CPF = 11122233309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class cliente{
+    nome;
+    cpf;
+}
 
-const cliente2Nome = "Alice";
-const cliente2CPF = 88822233309;
-const cliente2Agencia = 1001; 
-const cliente2Saldo = 0;
+class ContaCorrente{
+    agencia;
+    _saldo = 0;
 
-const cliente3Nome = "Alice";
-const cliente3CPF = 88822233309;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
-
-console.log(cliente1Nome);
-
-{ 
-    const nome;
-    const cpf;
-    const agencia;
-    const saldo;
+    sacar(valorSaque) {
+        if (this._saldo >= valorSaque) {
+            this._saldo -= valorSaque;
+           return `Saque realizado com sucesso, valor atual da conta R$${this._saldo} e o valor sacado foi R$${valorSaque}.`   
+        } else {
+           return `Saldo insuficiente, saldo atual R$${this._saldo}.`
+        }
     }
+
+    depositar(valorDeposito){
+        if (valorDeposito <= 0) 
+        {
+            return;
+        }
+        this._saldo += valorDeposito;
+        console.log(this._saldo);
+    }
+}
+
+const cliente1 = new cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+const cliente2 = new cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+const cliente3 = new cliente();
+cliente3.nome = "Alex";
+cliente3.cpf = 81233443309;
+
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+const contaCorrenteAlice = new ContaCorrente();
+contaCorrenteAlice.agencia = 1001;
+const contaCorrenteAlex = new ContaCorrente();
+contaCorrenteAlex.agencia = 1001;
+contaCorrenteRicardo.depositar(300);
+contaCorrenteRicardo.depositar(150);
+contaCorrenteRicardo.depositar(-150);
+
+console.log(contaCorrenteRicardo.sacar(50));
+console.log(contaCorrenteRicardo);
+
+
