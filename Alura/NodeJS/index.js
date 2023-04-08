9d8c5e26c194c2e54d07a7c3d7b9eb77f4c0810b
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-04-08 14:36:21 -0300

    prgredindo curso de node

--- Alura/NodeJS/index.js
+++ Alura/NodeJS/index.js
@@ -1,14 +1,12 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-//encadear métodos para colorir texto, cor de fundo e texto em negrito
-console.log(chalk.blue.bgWhite.bold('Alura'));
-//receber múltiplos argumentos
-console.log(chalk.blue('curso', 'de', 'Node.js'));
-//métodos aninhados
-console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
-// uso de template strings e placeholders
-console.log(`
-CPU: ${chalk.red('90%')}
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
+function pegaArquivo(caminhoArquivo) {
+    const encoding = 'utf-8'
+    fs.readFile(caminhoArquivo, encoding, (_, texto) =>{
+        console.log(chalk.green(texto));
+    });
+}
+
+
+pegaArquivo('./arquivos/texto.md');
