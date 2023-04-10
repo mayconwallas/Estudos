77a7f011f2ff7b65eefbdc60236e2fd3c9ce4c97
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-04-10 18:16:53 -0300

    commid do dia (sem criatividade para descrever)

---
+++ eucurso/logicaDeProgramacao/semana 01/aula02/index.js
@@ -0,0 +1,6 @@
+alert(10);
+alert(0);
+alert(5);
+
+let numero = 7;
+console.log(numero);
---
+++ eucurso/logicaDeProgramacao/semana 01/aula04/index.js
@@ -0,0 +1,8 @@
+
+let num = window.prompt('informe um numero:')
+if (num <= 5) {
+    alert('numero é igual ou menor que 5')            
+} else {
+    alert('nuemro é maior que 5')
+}
+
