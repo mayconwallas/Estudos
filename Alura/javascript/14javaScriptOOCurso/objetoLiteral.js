326b3b1585ce9b1af645e12c484aa9987d3a1a92
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-04-24 16:13:59 -0300

    progredindo estudo de classes e objetos

--- Alura/javascript/14javaScriptOOCurso/objetoLiteral.js
+++ Alura/javascript/14javaScriptOOCurso/objetoLiteral.js
@@ -2,16 +2,28 @@ const user = {
     nome: "Maycon",
     email: "M@W.com",
     nascimento: "2020/12/20",
-    role: "Supremo",
+    role: "Supremo user",
     ativo: true,
     exibirInfor: function() {
         console.log(this.nome, this.email, this.role)
     }
 }
+// const exibir = function() {
+//     console.log(this.nome, this.email, this.role)
+// }
+// const exibirNovo = exibir.bind(user);
+// exibirNovo();
 
-const exibir = function() {
-    console.log(this.nome, this.email, this.role)
+const admin = {
+    nome: "Wallas",
+    email: "M@W.com",
+    role: "Supremo admin",
+    criarCurso() {
+        console.log("curso criado!");
+    }
 }
 
-const exibirNovo = exibir.bind(user);
-exibirNovo();
+Object.setPrototypeOf(admin, user);
+admin.criarCurso();
+admin.exibirInfor();
+user.exibirInfor();
