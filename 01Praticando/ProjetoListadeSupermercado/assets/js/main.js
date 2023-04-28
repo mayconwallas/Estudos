9736a28970cf8336f36adef3400e4448a004c612
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-04-28 10:22:52 -0300

    selecionando h3

--- 01Praticando/ProjetoListadeSupermercado/assets/js/main.js
+++ 01Praticando/ProjetoListadeSupermercado/assets/js/main.js
@@ -7,10 +7,32 @@ function atualizarValorSelecionado() {
     const valorSelecionado = select.options[select.selectedIndex].value;
     const textoSelecionado = select.options[select.selectedIndex].text;
     const itemAdd = inputItem.value;
-    console.log(itemAdd);
-    if (valorSelecionado)
-    listaMercado.innerHTML += `<h3>${textoSelecionado}</h3><ul><li>${itemAdd}</li></ul>`;
-    inputItem.value = '';
+    const listaH3 = []; 
+
+
+    
+    function verificaExisteElemeno(listaH3, textoSelecionado, itemAdd) {
+      
+    }
+      
+      if (valorSelecionado) {
+        const novoH3 = document.createElement('h3');
+        const novoTextoH3 = document.createTextNode(textoSelecionado);
+        novoH3.appendChild(novoTextoH3);
+        
+        const novaLista = document.createElement('ul');
+        const novoItem = document.createElement('li');
+        const novoTextoItem = document.createTextNode(itemAdd);
+        novoItem.appendChild(novoTextoItem);
+        novaLista.appendChild(novoItem);
+        
+        listaMercado.appendChild(novoH3);
+        listaMercado.appendChild(novaLista);
+        listaH3.push(novoH3);// adiciona o novo h3 no no array listaH3
+        inputItem.value = '';
+      }
+      console.log(listaH3.length);
+      console.log(listaH3[0]);
     }
 
 form.addEventListener('submit', (event) => {
