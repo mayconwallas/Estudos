854f322e0adff6138c156ce33d25eb7f6bbdf3fd
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-04-28 16:20:26 -0300

    javascript bytbank

--- Alura/javascript/12ByteBank/Cliente.js
+++ Alura/javascript/12ByteBank/Cliente.js
@@ -8,4 +8,3 @@ export class Cliente{
         this._cpf = cpf;
     }
 }
-
--- Alura/javascript/12ByteBank/Conta.js
+++ Alura/javascript/12ByteBank/Conta.js
@@ -1,31 +1,46 @@
-export class Conta{
-
-    constructor(saldoInicial, cliente, agencia){
-        this.agencia = agencia;
-        this.cliente = cliente;
-        this._saldo = 0;
+export class Conta {
+    constructor(saldoInicial, cliente, agencia) {
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+       
     }
 
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
         }
     }
 
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        } 
-        this._saldo += valor;           
+    get cliente() {
+        return this._cliente;
     }
 
-    tranferir(valor, conta){
-        
+    get saldo() {
+        return this._saldo;
+    }
+
+    sacar(valor) {
+        let taxa = 1
+        return this._sacar(valor, taxa);
+    }
+
+    _sacar(valor, taxa){
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+
+        return 0;
+    }
+
+    depositar(valor) {
+        this._saldo += valor;
+    }
+
+    tranferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-        
     }
 }
