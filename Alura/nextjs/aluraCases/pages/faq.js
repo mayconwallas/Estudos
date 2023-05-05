4f69b4db9a8ed6feb947146e9f8712ce624df791
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-05-05 16:58:05 -0300

    aprendendo next.js

---
+++ Alura/nextjs/aluraCases/pages/faq.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link';
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Alura Cases - Faq Page</h1>
+            <Link href="/" passHref>
+                ir para Home
+            </Link>
+        </div>
+    )
+}
---
+++ Alura/nextjs/aluraCases/pages/index.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link';
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Alura Cases - Home Page</h1>
+            <Link href="/faq" passHref>
+                ir para FAQ
+            </Link>
+        </div>
+    )
+}
