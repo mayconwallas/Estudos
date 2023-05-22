b5a6841274ad91285577987f1a4172292a758240
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-05-22 15:34:53 -0300

    Estudando mais sobre react

---
+++ Alura/react/04ReactRouter/olamundo/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from './paginas/inicio';
+import SobreMim from './paginas/SobreMim'
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="sobremim" element={<SobreMim />}/>
+        <Route path="*" element={<div>Página não encontrada ERRO 404</div>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
