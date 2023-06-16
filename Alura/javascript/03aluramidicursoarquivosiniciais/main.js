3e5590f856fd6608fd85b310d8d11dd862525435
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-06-16 18:34:04 -0300

    estudando TypeScript

--- Alura/javascript/03aluramidicursoarquivosiniciais/main.js
+++ Alura/javascript/03aluramidicursoarquivosiniciais/main.js
@@ -1,31 +1,43 @@
-function tocaSom (idElementoAudio) {
-   const elemento = document.querySelector(seletorAudio); 
+// function tocaSom (idElementoAudio) {
+//    const elemento = document.querySelector(seletorAudio); 
 
-    if (elemento && elemento.localName === 'audio') {
-            elemento.play();
+//     if (elemento && elemento.localName === 'audio') {
+//             elemento.play();
             
-    } else {
-        console.log('Elemento não encontrado ou seletor inválido.')
-    }
+//     } else {
+//         console.log('Elemento não encontrado ou seletor inválido.')
+//     }
+// }
+
+// const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+//     const idAudio = `#som_${instrumento}`
+    
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     }
+//     tecla.onkeydown = function (evento) {
+//         if (evento.code === 'Space' || evento.code === 'Enter') {
+//             tecla.classList.add('ativa');
+//         }
+//     }
+//     tecla.onkeyup = function () {
+//         tecla.classList.remove('ativa');
+//     }
+// }
+
+
+
+
+function tocaSomPom() {
+   const pom = document.querySelector('#som_tecla_pom')
+   pom.play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-    
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`
-    
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
-    tecla.onkeydown = function (evento) {
-        if (evento.code === 'Space' || evento.code === 'Enter') {
-            tecla.classList.add('ativa');
-        }
-    }
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
-}
+// Restante do código
