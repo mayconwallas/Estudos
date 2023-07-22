d44570fde0af21a1c593746203be807dd0b2661f
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-07-22 05:45:00 -0300

    commit revisão
    revisando algumas coisas e treinando outras.

---
+++ DIO/primeirosPassosComJS/variaveis/declarandoVariaveis.js
@@ -0,0 +1,28 @@
+/* 
+    faça um programa para calcular o valor de uma viagem.
+    preço medio do combustivel.
+    gasto medio que o carro tem.
+    e a distancia da viagem.
+
+*/
+
+
+// let precoCombustivel = 5.63;
+// let mediaGstoCarro = 12;
+// let distanciaViagem = 250;
+
+// let quantosLitrovaiGastar = distanciaViagem / mediaGstoCarro;
+// let valorGstoViagem = quantosLitrovaiGastar * precoCombustivel;
+
+// console.log(valorGstoViagem.toFixed(2));
+
+
+
+
+function testando () {
+    const a = 1;
+    let b = 2;
+}
+testando();
+console.log(a);
+console.log(b);
