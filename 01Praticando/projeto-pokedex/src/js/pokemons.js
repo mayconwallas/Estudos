037676562a7efdf84d886931bc6d47fa7d8722c5
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-08-01 13:40:38 -0300

    commit melhorando projetos.
    ainda estou apenas arrumando os arquivos e fazendo pequenos estudos.

---
+++ 01Praticando/projeto-pokedex/src/js/pokemons.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+function rederizarPokemon() {
+    <ul class="listagem-pokemon">
+        <li class="cartao-pokemon">
+            <div class="informacoes">
+                <span>Bulbasaur</span>
+                <span>#001</span>
+            </div>
+
+            <img src="./src/imagens/bulbasaur.gif" alt="Bulbasaur" class="gif" />
+
+            <ul class="tipos">
+                <li class="tipo grama">Grama</li>
+                <li class="tipo veneno">Veneno</li>
+            </ul>
+
+            <p class="descricao">Há uma semente de planta em suas costas desde o dia que este Pókemon nasce. A
+                semente cresce lentamente.</p>
+        </li>
+    </ul>
+};
