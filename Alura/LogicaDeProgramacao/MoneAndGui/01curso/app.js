d0c56ab44099a8f3dad4d4b5f3aaa3e5a980eb32
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-08-22 12:40:22 -0300

    commit
    continuando os estudos de logica

--- Alura/LogicaDeProgramacao/MoneAndGui/01curso/app.js
+++ Alura/LogicaDeProgramacao/MoneAndGui/01curso/app.js
@@ -1,2 +1,20 @@
-alert('Boas vindas ao Jogo do número secreto.');
-let numeroSecreto = parseInt(Math.random() * (10 - 1) + 1);
+alert("Boas vindas ao Jogo do número secreto.")
+let numeroSecreto = parseInt(Math.random() * (10 - 1) + 1)
+let numEscolhido = 0
+let tentativas = 1
+console.log(numeroSecreto)
+while (numeroSecreto != numEscolhido) {
+  numEscolhido = parseInt(prompt("Escolha um número entre 1 e 10."))
+
+  if (numEscolhido == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`)
+  } else {
+    if (numeroSecreto > numEscolhido) {
+      alert(`O número secreto é maior que ${numEscolhido}`)
+    } else {
+      alert(`O número secreto é menor que ${numEscolhido}`)
+    }
+    tentativas++
+    console.log(tentativas)
+  }
+}
--- Alura/javascript/01trilhaJavaScriptBasica/01JavaScriptArrays/01IntroducaoArrays.js
+++ Alura/javascript/01trilhaJavaScriptBasica/01JavaScriptArrays/01IntroducaoArrays.js
@@ -9,8 +9,26 @@
 //const media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
 //console.log(media);
 
-const notas = [10, 6.5, 8, 7.5];
+// const notas = [10, 6.5, 8, 7.5];
 
-const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+// const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
-console.log(media);
+// console.log(media);
+
+const notas = [10, 6.5, 8, 7.5, 1, 2, 10, 5]; 
+
+function somandoArray(arrayComNotas) {
+    let element = 0;
+    for (let i = 0; i < arrayComNotas.length; i++) {
+      element += arrayComNotas[i]
+    }
+    return element
+}
+
+function media(array) {
+    const numSomado = somandoArray(array);
+    const media = numSomado / array.length;
+    return media;
+}
+
+console.log(media(notas));
